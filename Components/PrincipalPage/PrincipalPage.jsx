fe22cf1e8dcfff846552ec4ec13653e3c1c7c35a
Ivixx09 <estudio.xx23@gmail.com>
2023-01-03 16:42:58 -0300

    PrincipalPage added

---
+++ Components/PrincipalPage/PrincipalPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PrincipalPage = () => {
+  return (
+    <div className='bg-slate-50 bg pt-36 pb-36'>
+      <div className='flex justify-center py-10'>
+        <span className='font-hind text-6xl'>
+          Hi! i am Ivo Maydana
+        </span>
+        <img src="./1F44B.svg" className='w-14 h-14' alt="" />
+      </div>
+      <div className='flex justify-center'>
+        <span className='font-hind text-6xl '>
+          A FullStack web developer
+        </span>
+        <img src="./1F4BB.svg" className='w-14 h-14' alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default PrincipalPage
