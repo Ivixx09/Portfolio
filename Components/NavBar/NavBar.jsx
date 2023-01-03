6551ee94eeaa02fb008ef10ef794a3e1a44215d5
Ivixx09 <estudio.xx23@gmail.com>
2023-01-03 16:41:54 -0300

    change about & skill

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { useRouter } from "next/router";
+
 
 const NavBar = () => {
+  
+  const router = useRouter()
+  
+  function routerPushS () {
+  }
+  function routerPushA () {
+    router.push("/about")
+  }
+    function routerPushP () {
+    router.push("/proyects")
+  }
+  function routerPushC () {
+    router.push("/contact")
+  }
+  
+  
   return (
-    <div className='bg-slate-200 h-14'>
+    <div className='bg-slate-50 h-14'>
       <div className='flex h-full justify-between'>
         <div className=' flex items-center pl-4 font-hind'>
           <span>IVO MAYDANA</span>
         </div>
           <div className='flex items-center'>
+            <span className='px-2 font-hind' onClick={routerPushA}>About Me</span>
             <span className='px-2 font-hind'>Skills</span>
-            <span className='px-2 font-hind'>About me</span>
             <span className='px-2 font-hind'>Proyects</span>
             <span className='px-2 font-hind'>Contact me</span>
           </div>
